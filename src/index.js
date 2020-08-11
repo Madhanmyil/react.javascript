b239ad60bfaa99b1d7fc2b563bbd2e6376f0a60c
Madhan <47316868+Madhanmyil@users.noreply.github.com>
2020-08-11 16:01:57 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>bacon</li>
+      <li>Noodle</li>
+      <li>Mushrooms</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
